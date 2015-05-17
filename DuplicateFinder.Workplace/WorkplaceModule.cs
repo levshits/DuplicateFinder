2986d3a137b10b21c9b62f7432617a6d056faf6a
levshits <levshits@yahoo.com>
2015-05-17 19:17:04 +0300

    Advanced mode initial commit

---
+++ DuplicateFinder.Workplace/WorkplaceModule.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+using DuplicateFinder.Infrastructure;
+using DuplicateFinder.Workplace.Resources;
+using DuplicateFinder.Workplace.Views;
+using LocalizationCommons;
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace DuplicateFinder.Workplace
+{
+    [ModuleExport(typeof(WorkplaceModule))]
+    public class WorkplaceModule:IModule
+    {
+        [Import]
+        private ILocalizationService localizationService;
+        [Import] 
+        private IRegionManager regionManager;
+        public void Initialize()
+        {
+            localizationService.RegisterManager("Workplace", Resource.ResourceManager);
+            regionManager.RegisterViewWithRegion(RegionNames.WorkplaceRegion, typeof(WelcomeView));
+        }
+    }
+}
